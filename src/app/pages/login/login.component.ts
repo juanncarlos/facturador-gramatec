8b9bbe179c4ab7307d967b78a30f44b3758574b0
Juan Carlos <juann200913@gmail.com>
2023-04-17 21:29:57 -0500

    mi primer commit

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { UntypedFormGroup, FormControl, AbstractControl, UntypedFormBuilder, Validators} from '@angular/forms'; 
+import { EmailValidators } from 'ngx-validators'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LoginComponent {
+  public router: Router;
+  public form:UntypedFormGroup;
+  public email:AbstractControl;
+  public password:AbstractControl;
+
+  constructor(router:Router, fb:UntypedFormBuilder) {
+      this.router = router;
+      this.form = fb.group({
+          'email': ['', Validators.compose([Validators.required, EmailValidators.normal])],
+          'password': ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+      });
+
+      this.email = this.form.controls['email'];
+      this.password = this.form.controls['password'];
+  }
+
+  public onSubmit(values:Object):void {
+      if (this.form.valid) {
+          this.router.navigate(['/']);
+      }
+  }
+
+  ngAfterViewInit(){
+      document.getElementById('preloader').classList.add('hide');                 
+  }
+
+}
