d043a21a9b8cc122564d7e8d1e56337af4ee8cb9
Juan Carlos <juann200913@gmail.com>
2023-04-28 17:59:21 -0500

    terminando la vista de productos

--- src/app/pages/gestion-inventario/producto/producto.component.ts
+++ src/app/pages/gestion-inventario/producto/producto.component.ts
@@ -11,9 +11,25 @@ export class ProductoComponent implements OnInit {
   public estado = ['activo', 'inactivo'];
   public genderOption:string;
 
+
+  imagen: File;
+  imagenPreview: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  url: string|null|ArrayBuffer = './assets/banner1.jpg' 
+onFileSelected(files: FileList | null) {
+    if (files) {
+        var reader = new FileReader()
+        reader.readAsDataURL(files[0])
+        reader.onload = (event:Event) => {
+          let fileReader = event.target as FileReader
+          this.url = fileReader.result;
+        }
+    }
+  }
+   
 }
