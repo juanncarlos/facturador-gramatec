c96d85b7a0e64349e4c85ebb8c33d97fed86a4fa
juan carlos <juann200913@gmail.com>
2023-05-10 16:05:31 -0500

    tratando de mostrar una tabla en la busqueda

--- src/app/pages/gestion-venta/cotizacion03/cotizacion03.component.ts
+++ src/app/pages/gestion-venta/cotizacion03/cotizacion03.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cotizacion03.component.scss']
 })
 export class Cotizacion03Component implements OnInit {
+  originalData: any = ['opción 1', 'opción 2', 'opción 3', 'opción 4'];
+  searchTerm: any;
+  filteredData: any = [
+    {
+    name: 'juan',
+    email: 'juan@gmail.com'
+  },
+  {
+    name: 'carlos',
+    email: 'carlos@gmail.com'
+  }
+];
+  searchResults: any;
+  
 
+  options = ['opción 1', 'opción 2', 'opción 3', 'opción 4'];
+  
   constructor() { }
-
   ngOnInit(): void {
   }
+  filterData() {
+    this.filteredData = this.originalData.filter(element =>
+      element.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1 ||
+      element.email.toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1);
+  }
+
+  search() {
+    this.searchResults = this.options.filter(option =>
+      option.toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1);
+  }
+  select(option: string) {
+    this.searchTerm = option;
+    this.searchResults = [];
+  }
+  
+  
+  
 
 }
