ad1f6f45dadc25501a2b682a1fdd664a67e798cd
juan carlos <juann200913@gmail.com>
2023-06-03 17:18:33 -0500

    mejorando la lista de productos en ventas

--- src/app/pages/gestion-venta/cotizacion03/cotizacion03.component.ts
+++ src/app/pages/gestion-venta/cotizacion03/cotizacion03.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cotizacion03',
@@ -6,35 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cotizacion03.component.scss']
 })
 export class Cotizacion03Component  {
-    
-  productos: any[] = [
-    { id: 1, nombre: 'Producto 1', precio: 10 },
-    { id: 2, nombre: 'Producto 2', precio: 20 },
-    { id: 3, nombre: 'Producto 3', precio: 30 },
-    { id: 4, nombre: 'Producto 4', precio: 40 },
-    { id: 5, nombre: 'Producto 5', precio: 50 },
-    { id: 1, nombre: 'Producto 6', precio: 10 },
-    { id: 2, nombre: 'Producto 7', precio: 20 },
-    { id: 3, nombre: 'Producto 8', precio: 30 },
-    { id: 4, nombre: 'Producto 9', precio: 40 },
-    { id: 5, nombre: 'Producto 10', precio: 50 }
-  ];
 
-  /* nombres: string[] = ['Juan', 'María', 'Pedro', 'Luis', 'Ana']; */
-  cantidadMostrar: number = 3;
+  allProducts: string[] = ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4', 'Producto 5'];
+  filteredProducts: string[] = [];
+  selectedProducts: string[] = [];
+  productName: string = '';
 
-  productosFiltrados: any[] = [];
-  terminoBusqueda: string = '';
-  productosSeleccionados: any[] = [];
-
-  filtrarProductos() {
-    this.productosFiltrados = this.productos.filter(producto =>
-      producto.nombre.toLowerCase().includes(this.terminoBusqueda.toLowerCase())
+  filterProducts() {
+    this.filteredProducts = this.allProducts.filter((product) =>
+      product.toLowerCase().includes(this.productName.toLowerCase())
     );
   }
 
-  agregarProducto(producto: any) {
-    this.productosSeleccionados.push(producto);
+  clearFilter() {
+    setTimeout(() => {
+      this.filteredProducts = [];
+    }, 200);
   }
+
+  addProduct(product: string) {
+    const index = this.selectedProducts.indexOf(product);
+    if (index === -1) {
+      this.selectedProducts.push(product);
+    }
+  }
+
+  removeProduct(product: string) {
+    const index = this.selectedProducts.indexOf(product);
+    if (index !== -1) {
+      this.selectedProducts.splice(index, 1);
+    }
+  }
+
+ 
 
 }
